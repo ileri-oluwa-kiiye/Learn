8fc278946ad0a364f562909efc32b035e80af3c5
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2022-11-09 15:54:20 -0800

    Me and this javascript in one shoe

---
+++ Basics/index.js
@@ -0,0 +1,13 @@
+//To change an element with the id "demo"
+
+function changeTagById() {
+    document.getElementById("iddemo").innerHTML = "Paragraph changed.";
+}
+
+document.write("This is used to add a new paragraph. This should strictly only be used for testing.")
+
+function createWindowAlert(){
+    window.alert("Hi, I hope you're fine. Click 'OK' to close this alert.")
+}
+
+console.log("We console things to debug in JavaScript")
